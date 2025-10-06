675c5854df36a963b086fe9de5754dfd6df60b28
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-10-05 20:58:28 -0700

    Checking the specs

--- main.cpp
+++ main.cpp
@@ -21,26 +21,25 @@ int main()
     // Set the precision for floating point numbers
     cout << fixed << setprecision(2);
 
-    // Creating pointer to first chair object
-    Chair *chairPtr = new Chair;
+    // Creating pointer to first Chair object
+    Chair *chairPtr = new Chair();
     chairPtr->setLegs(4);
     chairPtr->setPrices(121.21, 232.32, 414.14);
     chairPtr->print();
+    delete chairPtr;
+    chairPtr = nullptr;
 
-    // Creating dynamic chair object with constructor
-    Chair *livingChair = new Chair(3);
-    livingChair->setPrices(525.25, 434.34, 252.52);
+    // Creating 3 double arrays to store the prices
+    double price[SIZE] = {525.25, 434.34, 252.52};
+
+    // Creating dynamic Chair object with constructor
+    Chair *livingChair = new Chair(3, price);
     livingChair->print();
     delete livingChair;
     livingChair = nullptr;
 
-    // Creating 3 double arrays to store the prices
-    double price1[SIZE] = {441.41, 552.52, 663.63};
-    double price2[SIZE] = {484.84, 959.59, 868.68};
-    double price3[SIZE] = {626.26, 515.15, 757.57};
-
     // Creating an array of 3 Chair objects
-    Chair collection[SIZE] = { Chair(4, price1), Chair(4, price2), Chair(4, price3) };
+    Chair collection[SIZE];
 
     // Display the data of all the Chair objects
     for (int i = 0; i < SIZE; i++)
